84238824f708cd0c93f7b3f9c61b7283c276ed74
Zakhar <zax.osipov2010@yandex.ru>
2021-01-08 15:11:49 +0300

    debounce plugin

---
+++ debounce.js
@@ -0,0 +1,12 @@
+export default {
+    install (Vue, options) {
+        Vue.prototype.$debounce = function (fn, delay) {
+            let timeout
+            return (function () {
+                const fnCall = () => { fn.apply(this, arguments) }
+                clearTimeout(timeout)
+                timeout = setTimeout(fnCall, ms)
+            })()
+        }
+    }
+}
