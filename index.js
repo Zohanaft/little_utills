2f73f2b8f9146b4ba3a8b9b83cb0eee7eebd5178
Zakhar <zax.osipov2010@yandex.ru>
2021-01-19 16:10:05 +0300

    fix

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ module.exports = {
         Vue.prototype.$debounce = function (fn, delay) {
             let timeout
             return (function () {
-                
-                if (fn.apply != undefined) {
-                    const fnCall = () => { fn.apply(this, arguments) }
-                    clearTimeout(timeout)
-                    timeout = setTimeout(fnCall, delay)
+                const fnCall = () => { 
+                    if (fn.apply) {
+                        return fn.apply(this, arguments)
+                    }
+                    else {
+                        return fn()
+                    }
                 }
-                else {
-                    const fnCall = () => { fn }
-                    clearTimeout(timeout)
-                    timeout = setTimeout(fnCall, delay)
-                }
-                
+                clearTimeout(timeout)
+                timeout = setTimeout(fnCall, delay)
             })()
         }
     }
